3b8669431be13a5658aedc3140b9a575c48f7fdb
Mark Storus <mark.storus@airbnb.com>
2014-10-01 23:10:24 -0700

    add customWidth option

--- src/index.js
+++ src/index.js
@@ -47,6 +47,7 @@ function svgicons2svgfont(glyphs, options) {
   options = options || {};
   options.fontName = options.fontName || 'iconfont';
   options.fixedWidth = options.fixedWidth || false;
+  options.customWidth = options.customWidth || false;
   options.ascent = options.ascent || 0;
   options.descent = options.descent || 0;
   options.round = options.round || 10e12;
@@ -205,7 +206,7 @@ function svgicons2svgfont(glyphs, options) {
 <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd" >\n\
 <svg xmlns="http://www.w3.org/2000/svg">\n\
 <defs>\n\
-  <font id="' + options.fontName + '" horiz-adv-x="' + fontWidth + '">\n\
+  <font id="' + options.fontName + '" horiz-adv-x="' + (options.customWidth || fontWidth) + '">\n\
     <font-face font-family="' + options.fontName + '"\n\
       units-per-em="' + fontHeight + '" ascent="' + (options.ascent || fontHeight - options.descent) + '"\n\
       descent="' + options.descent + '" />\n\
@@ -215,6 +216,9 @@ function svgicons2svgfont(glyphs, options) {
             , d = '';
           if(options.fixedWidth) {
             glyph.width = fontWidth;
+          }
+          if(options.customWidth) {
+            glyph.width = options.customWidth;
           }
           if(options.normalize) {
             glyph.height = fontHeight;
